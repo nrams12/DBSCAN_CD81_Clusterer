4a39bfcf0b828eccecb723a8a82da18521b13584
nrams12 <118292123+nrams12@users.noreply.github.com>
2026-07-21 09:54:18 -0500

    Update DBSCAN_Clusterer_CalculatorCD81.m
    updated comments

--- DBSCAN_Clusterer_CalculatorCD81.m
+++ DBSCAN_Clusterer_CalculatorCD81.m
@@ -77,16 +77,14 @@ for i =1:numberOfClusters
     Clusters(i).MaxDiameter = maxDiameter; 
 end
 %% NND Calculation
-   %This section of code calculates a new NND. Rather than measuring the
-   %distance of the center of a cluster to the nearest cluster center, this
-   %code will calculte the distance of all points in the cluster to all
-   %points of the nearest cluster. 
+   %This section of code calculates NND, measuring the
+   %distance of the center of a cluster to the nearest cluster center. 
 disp("Calculating NND");
 
 
 
 for k = 1:numberOfClusters %loop through the clusters
-    ClusterK = Clusters(k).XYG; %assign the cluster X and Y to varaibles
+    ClusterK = Clusters(k).XYG; %assign the cluster X and Y to variables
     Xk = ClusterK(:,1);
     Yk = ClusterK(:,2);
     centroid =[sum(Xk)/length(Xk),sum(Yk)/length(Yk)]; %find centroid by average of localizations in cluster
@@ -96,7 +94,7 @@ end
 
 distance = NaN(numberOfClusters,numberOfClusters); %build matrix that is as large as #cluster x #clusters 
 for j = 1:numberOfClusters %loop through the clusters
-    clusterCenter1 = Clusters(j).Centroid; %assign the cluster X and Y to varaibles
+    clusterCenter1 = Clusters(j).Centroid; %assign the cluster X and Y to variables
     ClusterJ = cleanClusters(j).XYG;
     X5 = ClusterJ(:,1);
     center1X = clusterCenter1(1);
